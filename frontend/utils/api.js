585cfe9ba580d61796b4aa19a47b0219c8773e5a
emilabz <emilabrahamz@gmail.com>
2025-03-30 21:28:08 +0530

    Update baseURL with Render URL

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const baseURL = "URL"; // Replace with your backend URL
+const baseURL = "https://rnative-task-manager-backend.onrender.com"; // Replace with your backend URL
 
 const api = axios.create({
   baseURL: baseURL,
